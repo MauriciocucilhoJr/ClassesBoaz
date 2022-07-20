cb7b54998b0bca67d9bfed601195ff6b0cb3b67f
Mauricio Cucilho Junior <108343343+MauriciocucilhoJr@users.noreply.github.com>
2022-07-19 22:26:11 -0300

    Add files via upload
    testando gitignore

---
+++ classes dos insetos/classes dos insetos/Program.cs
@@ -0,0 +1,22 @@
+﻿    using System;
+
+namespace classes_dos_insetos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Insetos Insetos1 = new("Pulga",4,2,"cinza");
+            Insetos Insetos2 = new("Aranha", 8, 0, "marrom");
+            Insetos Insetos3 = new("Centopeia", 100, 2, "vermelho");
+            Insetos Insetos4 = new("Grilo", 6, 2, "verde");
+
+            Console.WriteLine(Insetos1.ToString());
+            Console.WriteLine(Insetos2.ToString());
+            Console.WriteLine(Insetos3.ToString());
+            Console.WriteLine(Insetos4.ToString());
+        }
+
+        
+    }
+}
---
+++ classes dos insetos/classes dos insetos/Src.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes_dos_insetos
+{
+    internal class Insetos
+    {
+
+        public string Nome { get; set; }
+
+        public int Patas { get; set; }
+
+        private int Antenas { get; set; }
+
+        private string Cor { get; set; }  
+
+        public Insetos()    
+        {
+
+        }
+
+        public Insetos( string nome, int patas, int antenas, string cor)
+        {
+            Nome = nome;
+            Patas = patas;
+            Antenas = antenas;
+            Cor = cor;
+        }   
+         
+        public override string ToString()
+        {
+            return $"Inseto na sua casa agora mesmo: {Nome} com {Patas} patas {Antenas} antenas e de cor {Cor}";
+
+        }
+    }
+}
